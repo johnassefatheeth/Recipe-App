fe7c85d1a7a86e48dca7ea10eff2f65da6965b86
John Assefa <johnassefaforbussines@gmail.com>
2024-01-02 05:22:48 -0800

    ..

--- front-end/pages/Signup.vue
+++ front-end/pages/Signup.vue
@@ -1,10 +1,25 @@
 <template>
     <div>
-
-    </div>
+    <h1>Signup</h1>
+    <form @submit.prevent="signup">
+      <label>
+        Username:
+        <input v-model="username" type="text" required />
+      </label>
+      <label>
+        Password:
+        <input v-model="password" type="password" required />
+      </label>
+      <button type="submit">Signup</button>
+    </form>
+  </div>
 </template>
 
 <script setup>
+
+
+const username = ref('');
+const password = ref('');
 
 </script>
 
