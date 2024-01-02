a29335d0dc130b63a3c7dfdd969537947df7acb9
John Assefa <johnassefaforbussines@gmail.com>
2024-01-02 03:47:34 -0800

    ...

--- front-end/pages/UserProfile.vue
+++ front-end/pages/UserProfile.vue
@@ -1,21 +1,22 @@
 <template>
-<div class="p-12">
-    <div class="p-10 bg-zinc-400 flex">
-        <img class="m-4 rounded-full align-right" src="../assets/image/avatar.png" alt="userimage">
+<div class="p-12 flex-col">
+    <div class="p-10 bg-zinc-600 w-full flex m-4 mt-0 ">
+        <img class="m-4 rounded-full align-right w-52" src="../assets/image/avatar.png" alt="userimage">
             <div><h1 class="text-5xl m-4 ">name</h1>
-            <h1 class="text-2xl m-4 inline">@userName</h1>
+            <h1 class="text-2xl m-4 ">@userName</h1>
             </div>
     </div>
-    <div>
-        <h1>userName</h1>
-        <h1>email</h1>
-        <h1>created at</h1>
-    </div>
-    <div>
-         <h1>recipes</h1>
-        <h1>comments</h1>
-    </div>
-       
+    <div class="flex bg-zinc-700 m-4 h-96">
+        <div class="w-1/3 bg-gray-400 m-4 p-4 ">
+            <h1>userName</h1>
+            <h1>email</h1>
+            <h1>created at</h1>
+        </div>
+        <div class="w-2/3 bg-gray-400 m-4 p-4">
+            <h1>recipes</h1>
+            <h1>comments</h1>
+        </div>
+       </div>
     
 </div>
 </template>
