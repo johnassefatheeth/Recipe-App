1c429751054534ec9adbceccdef58a1f1bfb7446
John Assefa <johnassefaforbussines@gmail.com>
2024-01-02 13:01:28 -0800

    ..

--- front-end/pages/Signup.vue
+++ front-end/pages/Signup.vue
@@ -1,25 +1,22 @@
 <template>
     <div class="w-full flex h-screen justify-center  items-center bg-stone-700">
-    <div class="w-96 h-96 flex justify-center items-center bg-stone-500"> 
+    <div class="w-96 p-4 h-fit flex justify-center items-center bg-stone-500 rounded-xl"> 
     
     <br>
-    <form @submit.prevent="signup">
-      <label>
-        Username:
-        <input class="m-8" v-model="username" type="text" required />
-      </label>
+    <form class="w-full flex flex-col justify-center items-center" @submit.prevent="signup">
+        <h1 class="m-8 text-3xl">signup</h1>
+      
+        <input class="p-2 w-10/12 rounded-xl" placeholder="UserName" v-model="username" type="text" required />
+      
       <br>
-      <label>
-        email:
-        <input class="m-8" v-model="email" type="email" required />
-      </label>
+      
+        <input class="p-2 w-10/12 rounded-xl" placeholder="Email" v-model="email" type="email" required />
+      
       <br>
-      <label>
-        Password:
-        <input class="m-8" v-model="password" type="password" required />
-      </label>
+        <input class="p-2 w-10/12 rounded-xl" placeholder="password" v-model="password" type="password" required />
+      
       <br>
-      <button class="m-4" type="submit">Signup</button>
+      <button class="w-40 h-14 bg-slate-600 rounded-2xl text-white hover:bg-slate-900" type="submit">Signup</button>
     </form>
   </div>
   </div>
