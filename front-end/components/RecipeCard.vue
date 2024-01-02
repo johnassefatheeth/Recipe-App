f4892bc2f1de8e138b2959931ebeab72c59fadd1
John Assefa <johnassefaforbussines@gmail.com>
2024-01-02 13:16:07 -0800

    ..

--- front-end/components/RecipeCard.vue
+++ front-end/components/RecipeCard.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="h-96 bg-slate-600 w-72 m-4">
+    <div class="h-96 bg-slate-600 w-96 m-8">
         <img class="w-full" src="../assets/image/food.jfif" alt="food">
+        <div class="p-4">
+            <p class="text-lg">catagory</p>
+            <p class="text-3xl">title</p>
+            <p class="text-xl float-right relative bottom-0">preparation time</p>
+        </div>
 
     </div>
 </template>
