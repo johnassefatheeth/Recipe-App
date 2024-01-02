9ccedf97af5a8cf6602559958ab3ada2793e4ed4
John Assefa <johnassefaforbussines@gmail.com>
2024-01-02 12:46:01 -0800

    ..

--- front-end/pages/Login.vue
+++ front-end/pages/Login.vue
@@ -1,10 +1,30 @@
 <template>
-    <div>
-        <p>login</p>
-    </div>
+    <div class="w-full flex h-screen justify-center  items-center bg-stone-700">
+    <div class="w-96 h-96 flex justify-center items-center bg-stone-500"> 
+    
+    <br>
+    <form @submit.prevent="signup">
+      <label>
+        Username:
+        <input class="m-8" v-model="username" type="text" required />
+      </label>
+      <br>
+      <label>
+        Password:
+        <input class="m-8" v-model="password" type="password" required />
+      </label>
+      <br>
+      <button class="m-4" type="submit">Signup</button>
+    </form>
+  </div>
+  </div>
 </template>
 
 <script setup>
+
+
+const username = ref('');
+const password = ref('');
 
 </script>
 
