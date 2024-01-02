f5e242ba53eb00cdb3c799a50a0b6c9d31ea0528
John Assefa <johnassefaforbussines@gmail.com>
2024-01-02 03:33:02 -0800

    ...

--- front-end/pages/UserProfile.vue
+++ front-end/pages/UserProfile.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>
-
+<div class="p-12">
+    <div class="p-10 bg-zinc-400 flex">
+        <img class="m-4 rounded-full align-right" src="../assets/image/avatar.png" alt="userimage">
+            <div><h1 class="text-5xl m-4 ">name</h1>
+            <h1 class="text-2xl m-4 inline">@userName</h1>
+            </div>
     </div>
+    <div>
+        <h1>userName</h1>
+        <h1>email</h1>
+        <h1>created at</h1>
+    </div>
+    <div>
+         <h1>recipes</h1>
+        <h1>comments</h1>
+    </div>
+       
+    
+</div>
 </template>
 
 <script setup>
