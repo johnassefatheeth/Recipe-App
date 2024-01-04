b4f891cd4c3672e254fb41951ca1b133730cb20c
John Assefa <johnassefaforbussines@gmail.com>
2024-01-04 05:17:27 -0800

    ...

--- front-end/pages/Signup.vue
+++ front-end/pages/Signup.vue
@@ -3,25 +3,16 @@
     <div class="w-96 p-4 h-fit flex justify-center items-center bg-stone-500 rounded-xl"> 
     
     <br>
-    <form class="w-full flex flex-col justify-center items-center" @submit.prevent="signup">
-        <h1 class="m-8 text-3xl">signup</h1>
-
-        <input class="p-2 w-10/12 rounded-xl" placeholder="UserName" v-model="name" type="text" required />
-      
-      <br>
-      
-        <input class="p-2 w-10/12 rounded-xl" placeholder="UserName" v-model="username" type="text" required />
-      
-      <br>
-      
-        <input class="p-2 w-10/12 rounded-xl" placeholder="Email" v-model="email" type="email" required />
-      
-      <br>
-        <input class="p-2 w-10/12 rounded-xl" placeholder="password" v-model="password" type="password" required />
-      
-      <br>
-      <button class="w-40 h-14 bg-slate-600 rounded-2xl text-white hover:bg-slate-900" type="submit">Signup</button>
-    </form>
+    <VForm class="">
+        <VField
+          name="name"
+          :rules="isRequired"
+          class=""
+          placeholder="name"
+        />
+        <br>
+        <VErrorMessage name="name" class="help is-danger" />
+      </VForm>
   </div>
   </div>
 </template>
@@ -29,10 +20,12 @@
 <script setup>
 
 
-const username = ref('');
-const password = ref('');
-const email = ref('');
-const name=ref('');
+const isRequired = (value) => {
+  if (value && value.trim()) {
+    return true;
+  }
+  return "This is required right now!";
+};
 
 </script>
 
