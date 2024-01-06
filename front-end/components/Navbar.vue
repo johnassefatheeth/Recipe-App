c5c769808377b9963628bf56358a520252698c3a
John Assefa <johnassefaforbussines@gmail.com>
2024-01-06 12:07:36 -0800

    ...

--- front-end/components/Navbar.vue
+++ front-end/components/Navbar.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="fixed w-full h-28 bg-slate-400 top-0">
+        <ul class="flex justify-evenly items-center h-full">
+            <li> <NuxtLink to="/">Home</NuxtLink></li>
+            <li> <NuxtLink to="/about">about</NuxtLink></li>
+            <li> <NuxtLink to="/recipelisting">recipe</NuxtLink></li>
+            <li> <NuxtLink to="/login">login</NuxtLink></li>
+            <li> <NuxtLink to="/signup">signup</NuxtLink></li>
+            <li> <NuxtLink to="/userprofile">userprofile</NuxtLink></li>
+        </ul>
 
     </div>
 </template>
