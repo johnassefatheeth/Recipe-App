0f58edda7e28f536db7dd2d6f8690a014496a933
John Assefa <johnassefaforbussines@gmail.com>
2024-01-07 10:30:04 -0800

    ...

--- front-end/components/Navbar.vue
+++ front-end/components/Navbar.vue
@@ -14,27 +14,14 @@
 
 
         <div v-if="hoverOnBrowse" @mouseover="hoverOnBrowse = true" class="w-full h-fit bg-slate-400 p-6" >
-            <div class=" inline-block">
-            
             <ul>
-                <li class="m-4 w-full" @mouseover="hoverByTime=true" @mouseleave="hoverByTime = false" >by time
-                    
-                </li>
-                <li class="m-4">by catagory</li>
-                <li class="m-4">creator</li>
-                <li class="m-4">ingredients</li>
-                <li class="m-4">search recipe</li>
+                <li>catagory</li>
+                <li>preparation time</li>
+                <li>ingredient</li>
+                <li>creator</li>
+                <li>recipe title</li>
             </ul>
-        </div>
-            <div class="inline-block">
-                <ul v-if="hoverByTime">
-                        <li class="m-2"> - 15 min </li>
-                        <li class="m-2"> 15-30 min</li> 
-                        <li class="m-2"> 30-45 min</li>   
-                        <li class="m-2"> 45-60 min</li>  
-                        <li class="m-2"> 60 min - </li>              
-                    </ul></div>
-        
+
         </div>
 
     </div>
