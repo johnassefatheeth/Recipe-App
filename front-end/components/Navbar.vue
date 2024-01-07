71206a5941700af8e447e666bf280bc9c59bd113
John Assefa <johnassefaforbussines@gmail.com>
2024-01-07 09:37:42 -0800

    ...

--- front-end/components/Navbar.vue
+++ front-end/components/Navbar.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="fixed w-full h-28 bg-slate-400 top-0">
+    <div @mouseleave="hoverOnBrowse = false" class="fixed w-full h-28 bg-slate-400 top-0">
         <ul class="flex justify-evenly items-center h-full">
-            <li> <NuxtLink to="/">Home</NuxtLink></li>
-            <li> <NuxtLink to="/about">about</NuxtLink></li>
-            <li> <NuxtLink to="/recipelisting">recipe</NuxtLink></li>
-            <li >browse </li>
-            <li v-if="!signedin"> <NuxtLink to="/login">login</NuxtLink></li>
-            <li v-if="!signedin"> <NuxtLink to="/signup">signup</NuxtLink></li>
-            <li v-if="signedin"> <NuxtLink to="/userprofile">yourprofile</NuxtLink></li>
-            <li v-if="signedin"> <NuxtLink to="/AddRecipe">userprofile</NuxtLink> </li>
-            <li v-if="signedin"> <NuxtLink to="/Login" >logout</NuxtLink> </li>
+            <li @mouseover="hoverOnBrowse = false"> <NuxtLink to="/">Home</NuxtLink></li>
+            <li @mouseover="hoverOnBrowse = false"> <NuxtLink to="/about">about</NuxtLink></li>
+            <li @mouseover="hoverOnBrowse = false"> <NuxtLink to="/recipelisting">recipe</NuxtLink></li>
+            <li  @mouseover="hoverOnBrowse = true"  > browse</li>
+            <li @mouseover="hoverOnBrowse = false" v-if="!signedin"> <NuxtLink to="/login">login</NuxtLink></li>
+            <li @mouseover="hoverOnBrowse = false" v-if="!signedin"> <NuxtLink to="/signup">signup</NuxtLink></li>
+            <li @mouseover="hoverOnBrowse = false" v-if="signedin"> <NuxtLink to="/userprofile">yourprofile</NuxtLink></li>
+            <li @mouseover="hoverOnBrowse = false" v-if="signedin"> <NuxtLink to="/AddRecipe">Add Recipe</NuxtLink> </li>
+            <li @mouseover="hoverOnBrowse = false" v-if="signedin"> <NuxtLink to="/Login" >logout</NuxtLink> </li>
         </ul>
 
 
-        <div v-if="hoverOnBrowse">
+        <div v-if="hoverOnBrowse" @mouseover="hoverOnBrowse = true" class="w-full h-fit bg-slate-400 p-6" >
             <ul>
                 <li>by time</li>
                 <li>by catagory</li>
